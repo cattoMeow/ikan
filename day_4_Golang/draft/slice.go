0ab4ee0df27d22f67b03bedb7b27c4eea685de7e
cattoMeow <61348285+cattoMeow@users.noreply.github.com>
2022-08-25 11:40:37 +0700

    finishing slice

--- day_4_Golang/draft/slice.go
+++ day_4_Golang/draft/slice.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var buah = [4]string{"apple", "nanas", "melon", "alpukat"}
+	// var {nama-variabel} [{kapasitas}]{type-data}
+	// [...] variadic alias elastis arraynya, bebas diisi brppun
+	var buah4 = [4]string{"apple", "nanas", "melon", "alpukat"}
+	var buahUnlimited = [...]string{"apple", "nanas", "melon", "alpukat", "durian", "mangga"}
 
-	fmt.Println("buah: ", buah)
+	fmt.Println("buah: ", buah4)
+	fmt.Println("buah: ", buahUnlimited)
 }
